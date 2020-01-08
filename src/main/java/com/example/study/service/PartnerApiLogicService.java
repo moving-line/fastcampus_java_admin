d4056e60e5987bc6d0c818717e73028b42983549
moving-line <movinglinecheck@gmail.com>
2020-01-08 10:10:50 +0900

    Add partner CRUD
    - partner의 CRUD 기능 추가

---
+++ src/main/java/com/example/study/service/PartnerApiLogicService.java
@@ -0,0 +1,100 @@
+package com.example.study.service;
+
+import com.example.study.model.entity.Partner;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.PartnerApiRequest;
+import com.example.study.model.network.response.PartnerApiResponse;
+import com.example.study.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PartnerApiLogicService extends BaseService<PartnerApiRequest, PartnerApiResponse, Partner> {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Header<PartnerApiResponse> create(Header<PartnerApiRequest> request) {
+
+        PartnerApiRequest body = request.getData();
+        Partner partner = Partner.builder()
+                .name(body.getName())
+                .status(body.getStatus())
+                .address(body.getAddress())
+                .callCenter(body.getCallCenter())
+                .partnerNumber(body.getPartnerNumber())
+                .businessNumber(body.getBusinessNumber())
+                .ceoName(body.getCeoName())
+                .registeredAt(body.getRegisteredAt())
+                .category(categoryRepository.getOne(body.getCategoryId()))
+                .build();
+        Partner newPartner = baseRepository.save(partner);
+
+        return response(newPartner);
+    }
+
+    @Override
+    public Header<PartnerApiResponse> read(long id) {
+
+        return baseRepository.findById(id)
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<PartnerApiResponse> update(Header<PartnerApiRequest> request) {
+
+        PartnerApiRequest body = request.getData();
+        Optional<Partner> optionalPartner = baseRepository.findById(body.getId());
+
+        return optionalPartner
+                .map(partner -> partner
+                        .setName(body.getName())
+                        .setStatus(body.getStatus())
+                        .setAddress(body.getAddress())
+                        .setCallCenter(body.getCallCenter())
+                        .setPartnerNumber(body.getPartnerNumber())
+                        .setBusinessNumber(body.getBusinessNumber())
+                        .setCeoName(body.getCeoName())
+                        .setRegisteredAt(body.getRegisteredAt())
+                        .setUnregisteredAt(body.getUnregisteredAt())
+                        .setCategory(categoryRepository.getOne(body.getCategoryId()))
+                )
+                .map(updatedPartner -> baseRepository.save(updatedPartner))
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header delete(long id) {
+        Optional<Partner> optionalPartner = baseRepository.findById(id);
+
+        return optionalPartner
+                .map(partner -> {
+                    baseRepository.delete(partner);
+                    return Header.OK();
+                })
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    private Header<PartnerApiResponse> response(Partner partner) {
+        PartnerApiResponse body = PartnerApiResponse.builder()
+                .id(partner.getId())
+                .name(partner.getName())
+                .status(partner.getStatus())
+                .address(partner.getAddress())
+                .callCenter(partner.getCallCenter())
+                .partnerNumber(partner.getPartnerNumber())
+                .businessNumber(partner.getBusinessNumber())
+                .ceoName(partner.getCeoName())
+                .registeredAt(partner.getRegisteredAt())
+                .unregisteredAt(partner.getUnregisteredAt())
+                .categoryId(partner.getCategory().getId())
+                .build();
+
+        return Header.OK(body);
+    }
+}
